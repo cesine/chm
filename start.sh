4231eff4341139a706d13471794fff86548523b6
cesine <cesine@yahoo.com>
2015-08-17 23:10:31 -0400

    using index.js so most logic is in the js not in bash

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 #
 # Turns on (restarts) the server
 
-echo " Starting server"
-
-node_modules/pouchdb-server/bin/pouchdb-server &
-echo $! > pid
+echo " Starting server";
+ls databases || mkdir databases;
+cd databases;
+../lib/index.js &
+echo $! > ../pid;
+sleep 2;
+echo "Opening local chm at http://127.0.0.1:5984/_utils"
+open http://127.0.0.1:5984/_utils;
